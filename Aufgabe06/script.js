6a059f034d316661e6f5686709b3671c4f7cdcc0
Sarwi <64104107+Sarwi@users.noreply.github.com>
2020-06-06 10:10:21 +0200

    Teilaufgabe1

--- Aufgabe06/script.js
+++ Aufgabe06/script.js
@@ -4,166 +4,166 @@ var aufgabe06;
     let mandeln = {
         kategorie: "schokolade",
         bild: "gebrannteMandeln.jpg",
-        name: "gebrannte Mandeln€",
-        preis: 1.20,
+        name: "gebrannte Mandeln",
+        preis: 1.22,
         beschreibung: "nussig,zimtig"
     };
     let erdbeere = {
         kategorie: "schokolade",
         bild: "Erdbeere.png",
         name: "Erdbeerschokolade",
-        preis: 1.30,
+        preis: 1.33,
         beschreibung: "fruchtig, pricklend"
     };
     let karamel = {
         kategorie: "schokolade",
         bild: "Caramel.jpg",
         name: "Karamelschokolade",
-        preis: 1.00,
+        preis: 1.05,
         beschreibung: "cremig, süß"
     };
     let allerlei = {
         kategorie: "schokolade",
         bild: "AllerleiSchokolade.jpg",
         name: "Allerlei",
-        preis: 1.40,
+        preis: 1.44,
         beschreibung: "Beeren, kokosflocken, Nüsse"
     };
     let beeren = {
         kategorie: "schokolade",
         bild: "Beerenschokolade.jpg",
         name: "Beerenschokolade",
-        preis: 1.30,
+        preis: 1.33,
         beschreibung: "Heidelbeeren, Passionsfrucht"
     };
     let keks = {
         kategorie: "schokolade",
         bild: "Butterkeksschokolade.jpg",
         name: "Keksschokolade",
-        preis: 1.10,
+        preis: 1.11,
         beschreibung: "knusprig"
     };
     let nougat = {
         kategorie: "schokolade",
         bild: "Nougat.png",
         name: "Nougatschokolade",
-        preis: 1.20,
+        preis: 1.11,
         beschreibung: "nusiig, cremig"
     };
     let smarties = {
         kategorie: "schokolade",
         bild: "Smarties.jpg",
         name: "Smartieesschokolade",
-        preis: 1.20,
+        preis: 1.22,
         beschreibung: "zart, reichlich gefüllt"
     };
     let vollmilch = {
         kategorie: "schokolade",
         bild: "Vollmilch.jpg",
         name: "Vollmilchschokolade",
-        preis: 1.00,
+        preis: 1.04,
         beschreibung: "Der Klassiker unter der Schokolade"
     };
     let weisse = {
         kategorie: "schokolade",
         bild: "Weisse.jpg",
-        name: "Weißeschokolade ",
-        preis: 1.00,
+        name: "Weißeschokolade",
+        preis: 1.09,
         beschreibung: "cremig, vanillearoma"
     };
     let bitter = {
         kategorie: "schokolade",
         bild: "Zartbitter.jpg",
         name: "Zartbitterschokolade",
-        preis: 1.00,
+        preis: 1.09,
         beschreibung: "zart, bitter, knackig"
     };
     let nuss = {
         kategorie: "schokolade",
         bild: "Nussschokolade.jpg",
         name: "Nussschokolade",
-        preis: 1.20,
+        preis: 1.22,
         beschreibung: "nussig, cremig, knackig"
     };
     let cola = {
-        kategorie: "schokolade",
+        kategorie: "fruchtgummi",
         bild: "Cola.jpg",
         name: "Cola Fläschchen",
-        preis: 1.20,
+        preis: 1.22,
         beschreibung: "Schmeckt wie eine kalte Cola an heißen Sommer Tagen"
     };
     let schnecken = {
-        kategorie: "schokolade",
+        kategorie: "fruchtgummi",
         bild: "Fruchschnecken.jpg",
         name: "Fruchtschnecke",
-        preis: 1.10,
+        preis: 1.11,
         beschreibung: "so schön flutschig, wie Schneckenschleim"
     };
     let weingummi = {
-        kategorie: "schokolade",
+        kategorie: "fruchtgummi",
         bild: "Fruchtige_Weingummi.jpg",
-        name: "Weingummi",
+        name: "Weingummi ",
         preis: 1.33,
         beschreibung: "sorgen für ein beschwipstes Gefühl"
     };
     let lachgummis = {
-        kategorie: "schokolade",
+        kategorie: "fruchtgummi",
         bild: "Lachgummis.jpg",
         name: "Lachgummis",
-        preis: 1.20,
+        preis: 1.22,
         beschreibung: "Sorgen für gute Laune"
     };
     let lakritze = {
-        kategorie: "schokolade",
+        kategorie: "fruchtgummi",
         bild: "Lakritze.jpg",
         name: "Lakritze",
-        preis: 0.90,
+        preis: 0.99,
         beschreibung: "Einfach nur lecker!"
     };
     let obstsalat = {
-        kategorie: "schokolade",
+        kategorie: "fruchtgummi",
         bild: "Obstsalat.jpg",
         name: "Obstsalat",
-        preis: 1.20,
+        preis: 1.22,
         beschreibung: "Für etwas Fruchtiges zwischendurch unser fruchtiger Obstsalat"
     };
     let rainbow = {
-        kategorie: "schokolade",
+        kategorie: "fruchtgummi",
         bild: "Rainbow.jpg",
         name: "Rainbow gummi",
-        preis: 1.00,
+        preis: 1.09,
         beschreibung: "Sorgt für mehr Farbe in deinem Leben"
     };
     let schlangen = {
-        kategorie: "schokolade",
+        kategorie: "fruchtgummi",
         bild: "Schlangen.jpg",
         name: "Gummischlangen",
-        preis: 1.20,
+        preis: 1.22,
         beschreibung: "Nichts für Angsthasen"
     };
     let schlümpfe = {
-        kategorie: "schokolade",
+        kategorie: "fruchtgummi",
         bild: "Schlumpfe1.jpg",
         name: "Schlümpfe",
-        preis: 1.00,
+        preis: 1.09,
         beschreibung: "sorgt für schöne Kindheitserinnerungen"
     };
     let superhirn = {
-        kategorie: "schokolade",
+        kategorie: "fruchtgummi",
         bild: "Superhirn.jpg",
         name: "Gummihirn",
-        preis: 1.50,
+        preis: 1.55,
         beschreibung: "erhöht IQ"
     };
     let wassermelone = {
-        kategorie: "schokolade",
+        kategorie: "fruchtgummi",
         bild: "Wassermelone.jpg",
-        name: "Wassermelone",
+        name: "Wassermelone ",
         preis: 1.05,
         beschreibung: "Sorgt für Sommergefühle"
     };
     let schnuller = {
-        kategorie: "schokolade",
+        kategorie: "fruchtgummi",
         bild: "Schnuller.jpg",
         name: "Schnuller",
         preis: 1.25,
@@ -171,38 +171,50 @@ var aufgabe06;
     };
     let süßigkeit = [mandeln, erdbeere, karamel, allerlei, beeren, keks, nougat, smarties, vollmilch, weisse, bitter, nuss, cola, schnecken, weingummi, lachgummis, lakritze, obstsalat, rainbow, schlangen, schlümpfe, superhirn, wassermelone, schnuller];
     for (let index = 0; index < süßigkeit.length; index++) {
-        //Schokolade
         //div erstellen 
         let newDiv = document.createElement("div");
         newDiv.id = "produkt" + index;
-        document.getElementById("produkt")?.appendChild(newDiv);
+        document.getElementById("schokolade")?.appendChild(newDiv);
         //Bild 
-        let imgSchokolade = document.createElement("img");
-        imgSchokolade.src = süßigkeit[index].bild;
-        document.getElementById("produkt" + index)?.appendChild(imgSchokolade);
+        let imgSüßigkeit = document.createElement("img");
+        imgSüßigkeit.src = süßigkeit[index].bild;
+        document.getElementById("produkt" + index)?.appendChild(imgSüßigkeit);
         //Name
-        let nameSchokolade = document.createElement("p");
-        nameSchokolade.innerHTML = süßigkeit[index].name;
-        document.getElementById("produkt" + index)?.appendChild(nameSchokolade);
+        let nameSüßigkeit = document.createElement("p");
+        nameSüßigkeit.innerHTML = süßigkeit[index].name;
+        document.getElementById("produkt" + index)?.appendChild(nameSüßigkeit);
+        let preisSüßigkeit = document.createElement("p");
+        preisSüßigkeit.innerHTML = süßigkeit[index].preis + "€";
+        document.getElementById("produkt" + index)?.appendChild(preisSüßigkeit);
         //Beschreibung
-        let beschreibungSchokolade = document.createElement("p");
-        beschreibungSchokolade.innerHTML = süßigkeit[index].beschreibung;
-        document.getElementById("produkt" + index)?.appendChild(beschreibungSchokolade);
+        let beschreibungSüßigkeit = document.createElement("p");
+        beschreibungSüßigkeit.innerHTML = süßigkeit[index].beschreibung;
+        document.getElementById("produkt" + index)?.appendChild(beschreibungSüßigkeit);
         //in den Einkaufswagen button 
         let indenEinkaufwagen = document.createElement("button");
         indenEinkaufwagen.innerHTML = "in den Einkausfwagen";
         document.getElementById("produkt" + index)?.appendChild(indenEinkaufwagen);
+        indenEinkaufwagen.addEventListener("click", wagenButton);
+        indenEinkaufwagen.setAttribute("preis", süßigkeit[index].preis.toString());
         switch (süßigkeit[index].kategorie) {
-            case schokolade:
-                let getContainerschokolade = document.getElementById("schokolade");
-                getContainerschokolade.appendChild(newDiv);
+            case "schokolade":
+                let getContainerSchokolade = document.getElementById("schokolade");
+                getContainerSchokolade.appendChild(newDiv);
                 break;
-            case fruchtgummi:
-                let getContainerfruchtgummi = document.getElementById("fruchtgummi");
-                getContainerfruchtgummi.appendChild(newDiv);
+            case "fruchtgummi":
+                let getContainerFruchtgummi = document.getElementById("fruchtgummi");
+                getContainerFruchtgummi.appendChild(newDiv);
                 break;
             default:
                 break;
+        }
+        let einkaufZaehler = 0;
+        let preis = 0;
+        function wagenButton(_event) {
+            einkaufZaehler++;
+            console.log(einkaufZaehler);
+            preis += parseFloat(_event.target?.getAttribute("preis"));
+            console.log(preis);
         }
     }
 })(aufgabe06 || (aufgabe06 = {}));
--- Aufgabe06/script.ts
+++ Aufgabe06/script.ts
@@ -12,8 +12,8 @@ namespace aufgabe06 {
     let mandeln: Süßigkeit = {
         kategorie: "schokolade",
         bild: "gebrannteMandeln.jpg",
-        name: "gebrannte Mandeln€",
-        preis: 1.20,
+        name: "gebrannte Mandeln",
+        preis: 1.22,
         beschreibung: "nussig,zimtig"
 
     };
@@ -21,165 +21,165 @@ namespace aufgabe06 {
         kategorie: "schokolade",
         bild: "Erdbeere.png",
         name: "Erdbeerschokolade",
-        preis: 1.30,
+        preis: 1.33,
         beschreibung: "fruchtig, pricklend"
     };
     let karamel: Süßigkeit = {
         kategorie: "schokolade",
         bild: "Caramel.jpg",
         name: "Karamelschokolade",
-        preis: 1.00,
+        preis: 1.05,
         beschreibung: "cremig, süß"
     };
     let allerlei: Süßigkeit = {
         kategorie: "schokolade",
         bild: "AllerleiSchokolade.jpg",
         name: "Allerlei",
-        preis: 1.40,
+        preis: 1.44,
         beschreibung: "Beeren, kokosflocken, Nüsse"
     };
     let beeren: Süßigkeit = {
         kategorie: "schokolade",
         bild: "Beerenschokolade.jpg",
         name: "Beerenschokolade",
-        preis: 1.30,
+        preis: 1.33,
         beschreibung: "Heidelbeeren, Passionsfrucht"
     };
     let keks: Süßigkeit = {
         kategorie: "schokolade",
         bild: "Butterkeksschokolade.jpg",
         name: "Keksschokolade",
-        preis: 1.10,
+        preis: 1.11,
         beschreibung: "knusprig"
     };
     let nougat: Süßigkeit = {
         kategorie: "schokolade",
         bild: "Nougat.png",
         name: "Nougatschokolade",
-        preis: 1.20,
+        preis: 1.11,
         beschreibung: "nusiig, cremig"
     };
     let smarties: Süßigkeit = {
         kategorie: "schokolade",
         bild: "Smarties.jpg",
         name: "Smartieesschokolade",
-        preis: 1.20,
+        preis: 1.22,
         beschreibung: "zart, reichlich gefüllt"
     };
     let vollmilch: Süßigkeit = {
         kategorie: "schokolade",
         bild: "Vollmilch.jpg",
         name: "Vollmilchschokolade",
-        preis: 1.00,
+        preis: 1.04,
         beschreibung: "Der Klassiker unter der Schokolade"
     };
     let weisse: Süßigkeit = {
         kategorie: "schokolade",
         bild: "Weisse.jpg",
-        name: "Weißeschokolade ",
-        preis: 1.00,
+        name: "Weißeschokolade",
+        preis: 1.09,
         beschreibung: "cremig, vanillearoma"
     };
     let bitter: Süßigkeit = {
         kategorie: "schokolade",
         bild: "Zartbitter.jpg",
         name: "Zartbitterschokolade",
-        preis: 1.00,
+        preis: 1.09,
         beschreibung: "zart, bitter, knackig"
     };
     let nuss: Süßigkeit = {
         kategorie: "schokolade",
         bild: "Nussschokolade.jpg",
         name: "Nussschokolade",
-        preis: 1.20,
+        preis: 1.22,
         beschreibung: "nussig, cremig, knackig"
     };
+
     let cola: Süßigkeit = {
-        kategorie: "schokolade",
+        kategorie: "fruchtgummi",
         bild: "Cola.jpg",
         name: "Cola Fläschchen",
-        preis: 1.20,
+        preis: 1.22,
         beschreibung: "Schmeckt wie eine kalte Cola an heißen Sommer Tagen"
     };
     let schnecken: Süßigkeit = {
-        kategorie: "schokolade",
+        kategorie: "fruchtgummi",
         bild: "Fruchschnecken.jpg",
         name: "Fruchtschnecke",
-        preis: 1.10,
+        preis: 1.11,
         beschreibung: "so schön flutschig, wie Schneckenschleim"
     };
     let weingummi: Süßigkeit = {
-        kategorie: "schokolade",
+        kategorie: "fruchtgummi",
         bild: "Fruchtige_Weingummi.jpg",
-        name: "Weingummi",
+        name: "Weingummi ",
         preis: 1.33,
         beschreibung: "sorgen für ein beschwipstes Gefühl"
     };
     let lachgummis: Süßigkeit = {
-        kategorie: "schokolade",
+        kategorie: "fruchtgummi",
         bild: "Lachgummis.jpg",
         name: "Lachgummis",
-        preis: 1.20,
+        preis: 1.22,
         beschreibung: "Sorgen für gute Laune"
     };
     let lakritze: Süßigkeit = {
-        kategorie: "schokolade",
+        kategorie: "fruchtgummi",
         bild: "Lakritze.jpg",
         name: "Lakritze",
-        preis: 0.90,
+        preis: 0.99,
         beschreibung: "Einfach nur lecker!"
     };
     let obstsalat: Süßigkeit = {
-        kategorie: "schokolade",
+        kategorie: "fruchtgummi",
         bild: "Obstsalat.jpg",
         name: "Obstsalat",
-        preis: 1.20,
+        preis: 1.22,
         beschreibung: "Für etwas Fruchtiges zwischendurch unser fruchtiger Obstsalat"
     
     };
     let rainbow: Süßigkeit = {
-        kategorie: "schokolade",
+        kategorie: "fruchtgummi",
         bild: "Rainbow.jpg",
         name: "Rainbow gummi",
-        preis: 1.00,
+        preis: 1.09,
         beschreibung: "Sorgt für mehr Farbe in deinem Leben"
     };
     let schlangen: Süßigkeit = {
-        kategorie: "schokolade",
+        kategorie: "fruchtgummi",
         bild: "Schlangen.jpg",
         name: "Gummischlangen",
-        preis: 1.20,
+        preis: 1.22,
         beschreibung: "Nichts für Angsthasen"
     };
     let schlümpfe: Süßigkeit = {
-        kategorie: "schokolade",
+        kategorie: "fruchtgummi",
         bild: "Schlumpfe1.jpg",
         name: "Schlümpfe",
-        preis: 1.00,
+        preis: 1.09,
         beschreibung: "sorgt für schöne Kindheitserinnerungen"
     };
     let superhirn: Süßigkeit = {
-        kategorie: "schokolade",
+        kategorie: "fruchtgummi",
         bild: "Superhirn.jpg",
         name: "Gummihirn",
-        preis: 1.50,
+        preis: 1.55,
         beschreibung: "erhöht IQ"
     };
     let wassermelone: Süßigkeit = {
-        kategorie: "schokolade",
+        kategorie: "fruchtgummi",
         bild: "Wassermelone.jpg",
-        name: "Wassermelone",
+        name: "Wassermelone ",
         preis: 1.05,
         beschreibung: "Sorgt für Sommergefühle"
     };
     let schnuller: Süßigkeit = {
-        kategorie: "schokolade",
+        kategorie: "fruchtgummi",
         bild: "Schnuller.jpg",
         name: "Schnuller",
         preis: 1.25,
         beschreibung: "Schweigen ist Gold"
     };
-    
 
     let süßigkeit: Süßigkeit[] = [mandeln, erdbeere, karamel, allerlei, beeren, keks, nougat, smarties, vollmilch, weisse, bitter, nuss, cola, schnecken, weingummi, lachgummis, lakritze, obstsalat, rainbow, schlangen, schlümpfe, superhirn, wassermelone, schnuller];
 
@@ -188,49 +188,61 @@ namespace aufgabe06 {
         //div erstellen 
         let newDiv: HTMLDivElement = document.createElement("div");
         newDiv.id = "produkt" + index;
-        document.getElementById("produkt")?.appendChild(newDiv);
+        document.getElementById("schokolade")?.appendChild(newDiv);
 
 
         //Bild 
-        let imgSchokolade: HTMLImageElement = document.createElement("img");
-        imgSchokolade.src = süßigkeit[index].bild;
-        document.getElementById("produkt" + index)?.appendChild(imgSchokolade);
+        let imgSüßigkeit: HTMLImageElement = document.createElement("img");
+        imgSüßigkeit.src = süßigkeit[index].bild;
+        document.getElementById("produkt" + index)?.appendChild(imgSüßigkeit);
 
         //Name
-        let nameSchokolade: HTMLParagraphElement = document.createElement("p");
-        nameSchokolade.innerHTML = süßigkeit[index].name;
-        document.getElementById("produkt" + index)?.appendChild(nameSchokolade);
+        let nameSüßigkeit: HTMLParagraphElement = document.createElement("p");
+        nameSüßigkeit.innerHTML = süßigkeit[index].name;
+        document.getElementById("produkt" + index)?.appendChild(nameSüßigkeit);
+
+        let preisSüßigkeit: HTMLParagraphElement = document.createElement("p");
+        preisSüßigkeit.innerHTML = süßigkeit[index].preis + "€";
+        document.getElementById("produkt" + index)?.appendChild(preisSüßigkeit);
 
         //Beschreibung
-        let beschreibungSchokolade: HTMLParagraphElement = document.createElement("p");
-        beschreibungSchokolade.innerHTML = süßigkeit[index].beschreibung;
-        document.getElementById("produkt" + index)?.appendChild(beschreibungSchokolade);
+        let beschreibungSüßigkeit: HTMLParagraphElement = document.createElement("p");
+        beschreibungSüßigkeit.innerHTML = süßigkeit[index].beschreibung;
+        document.getElementById("produkt" + index)?.appendChild(beschreibungSüßigkeit);
 
         //in den Einkaufswagen button 
         let indenEinkaufwagen: HTMLButtonElement = document.createElement("button");
         indenEinkaufwagen.innerHTML = "in den Einkausfwagen";
         document.getElementById("produkt" + index)?.appendChild(indenEinkaufwagen);
-        
+        indenEinkaufwagen.addEventListener("click", wagenButton);
+        indenEinkaufwagen.setAttribute("preis", süßigkeit[index].preis.toString());
 
         switch (süßigkeit[index].kategorie) {
-            case schokolade:
-                let getContainerschokolade: HTMLElement = document.getElementById("schokolade")!;
-                getContainerschokolade.appendChild(newDiv);
+            case "schokolade":
+                let getContainerSchokolade: HTMLElement = document.getElementById("schokolade")!;
+                getContainerSchokolade.appendChild(newDiv);
                 break;
-                case fruchtgummi:
-                    let getContainerfruchtgummi: HTMLElement = document.getElementById("fruchtgummi")!;
-                    getContainerfruchtgummi.appendChild(newDiv);
-                    break;
-                    default:
-                        break;
+
+            case "fruchtgummi":
+                let getContainerFruchtgummi: HTMLElement = document.getElementById("fruchtgummi")!;
+                getContainerFruchtgummi.appendChild(newDiv);
+                break;
+                default:
+                break;
+        }
+        let einkaufZaehler: number = 0;
+        let preis: number = 0;
+
+        function wagenButton(_event: Event): void {
+            einkaufZaehler++;
+            console.log(einkaufZaehler);
+
+            preis += parseFloat((<HTMLButtonElement>_event.target)?.getAttribute("preis")!);
+            console.log(preis);
         }
     }
 }
 
-
-
     
 
-
-
-
+    
