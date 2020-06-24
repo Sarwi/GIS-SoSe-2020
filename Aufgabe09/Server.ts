f496dcdbecd40973512e874938543ca26dcae5e9
Sarwi <64104107+Sarwi@users.noreply.github.com>
2020-06-24 15:58:21 +0200

    update

--- Aufgabe09/Server.ts
+++ Aufgabe09/Server.ts
@@ -1,42 +1,37 @@
 import * as Http from "http";
 import * as Url from "url";
 
-export namespace A09Server {
-  console.log("Starting server");
-  let port: number = Number(process.env.PORT);
-  if (!port)
-    port = 8100;
+export namespace A08Server {
+console.log("Starting server");
+let port: number = Number(process.env.PORT);
+if (!port)
+port = 8100;
 
-  let server: Http.Server = Http.createServer();
-  server.addListener("request", handleRequest);
-  server.addListener("listening", handleListen);
-  server.listen(port);
+let server: Http.Server = Http.createServer();
+server.addListener("request", handleRequest);
+server.addListener("listening", handleListen);
+server.listen(port);
 
-  function handleListen(): void {
-    console.log("Listening");
-  }
+function handleListen(): void {
+console.log("Listening");
+}
 
-  function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
-    
+function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
+console.log("I hear voices!");
 
-    _response.setHeader("content-type", "text/html; charset=utf-8");
-    _response.setHeader("Access-Control-Allow-Origin", "*");
+_response.setHeader("content-type", "text/html; charset=utf-8");
+_response.setHeader("Access-Control-Allow-Origin", "*");
 
-    if (_request.url) {
-        let url: Url.UrlWithParsedQuery = Url.parse(_request.url, true);
-        let path: string | null = url.pathname;
+if (_request.url) {
+let url: Url.UrlWithParsedQuery = Url.parse(_request.url, true);
+for (let key in url.query) {
+_response.write(key + ":" + url.query[key] + "<br/>");
 
-        if (path == "/html") {
-        for (let key in url.query) {
-            _response.write(key + ": " + url.query[key] + "<br/>");
-        }
-    }
-  
-    else if (path == "/json") {
-      let jsonString: string = JSON.stringify(url.query);
-      _response.write(jsonString);
-    }
-  }
-    _response.end();
-  }
+}
+let jsonString: string = JSON.stringify(url.query);
+_response.write(jsonString);
+}
+
+_response.end();
+}
 }
