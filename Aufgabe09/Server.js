646f1ae985e87f55795a6fb759de59b91aba4b1c
Sarwi <64104107+Sarwi@users.noreply.github.com>
2020-06-24 16:07:41 +0200

    update

--- Aufgabe09/Server.js
+++ Aufgabe09/Server.js
@@ -1,10 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.A09Server = void 0;
+exports.A08Server = void 0;
 const Http = require("http");
 const Url = require("url");
-var A09Server;
-(function (A09Server) {
+var A08Server;
+(function (A08Server) {
     console.log("Starting server");
     let port = Number(process.env.PORT);
     if (!port)
@@ -17,22 +17,18 @@ var A09Server;
         console.log("Listening");
     }
     function handleRequest(_request, _response) {
+        console.log("I hear voices!");
         _response.setHeader("content-type", "text/html; charset=utf-8");
         _response.setHeader("Access-Control-Allow-Origin", "*");
         if (_request.url) {
             let url = Url.parse(_request.url, true);
-            let path = url.pathname;
-            if (path == "/html") {
-                for (let key in url.query) {
-                    _response.write(key + ": " + url.query[key] + "<br/>");
-                }
+            for (let key in url.query) {
+                _response.write(key + ":" + url.query[key] + "<br/>");
             }
-            else if (path == "/json") {
-                let jsonString = JSON.stringify(url.query);
-                _response.write(jsonString);
-            }
+            let jsonString = JSON.stringify(url.query);
+            _response.write(jsonString);
         }
         _response.end();
     }
-})(A09Server = exports.A09Server || (exports.A09Server = {}));
+})(A08Server = exports.A08Server || (exports.A08Server = {}));
 //# sourceMappingURL=Server.js.map
